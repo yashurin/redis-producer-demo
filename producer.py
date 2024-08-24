4bbb69a01cc776a84310605dcf2c11be0a9bab3e
Andrey Yashurin <yashurin@hotmail.com>
2024-08-24 16:09:13 +0200

    update initialisation

--- producer.py
+++ producer.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import json
-from redis.cluster import RedisCluster
+from redis.cluster import RedisCluster as Redis
 import redis
 import time
 import logging
@@ -18,9 +18,9 @@ class RedisStreamProducer:
         """Initialize the RedisStreamProducer with the stream name and Redis connection details."""
         self.stream_name = stream_name
         startup_nodes = [{"host": "10.128.0.3", "port": 6379}]
-        self.redis_client = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
-        redis_host = startup_nodes[0]["host"]
-        redis_port = startup_nodes[0]["port"]
+        self.redis_client = Redis(host="10.128.0.3", port=6379, decode_responses=True)
+        redis_host = "10.128.0.3" # startup_nodes[0]["host"]
+        redis_port = 6379 #startup_nodes[0]["port"]
 
         try:
             self.redis_client.ping()
